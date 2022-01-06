6954ea11449cd91f8edc93662faf5e4d06ef5432
goosewobbler <goosewobbler@pm.me>
2022-01-06 22:12:14 +0000

    remove waitUntilTextExists

--- test/application.spec.ts
+++ test/application.spec.ts
@@ -1,31 +1,3 @@
-import { Browser, WaitUntilOptions } from 'webdriverio';
-
-async function waitUntilTextExists(
-  this: Browser<'async'>,
-  selector: string,
-  text: string,
-  timeout: Partial<WaitUntilOptions>,
-) {
-  try {
-    await this.waitUntil(async () => {
-      const elem = await browser.$(selector);
-      if (!(await elem.isExisting())) {
-        return false;
-      }
-      const selectorText = await elem.getText();
-      return Array.isArray(selectorText)
-        ? selectorText.some((s: string[]) => s.includes(text))
-        : selectorText.includes(text);
-    }, timeout || 5000);
-  } catch (error) {
-    throw new Error(`waitUntilTextExists error: ${(error as Error).message}`);
-  }
-}
-
-before(() => {
-  browser.addCommand('waitUntilTextExists', waitUntilTextExists);
-});
-
 describe('application loading', () => {
   describe('App', () => {
     it('should launch the application', async () => {
