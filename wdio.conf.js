07cf3570ad3a5b3a67f6ca5013363c0f2da9e812
goosewobbler <goosewobbler@pm.me>
2022-01-06 22:02:34 +0000

    clean conf up

--- wdio.conf.js
+++ wdio.conf.js
@@ -18,35 +18,25 @@ const config = {
           port: 9519,
           logFileName: 'wdio-chromedriver.log',
         },
-        // args: ['--silent'],
       },
     ],
   ],
   capabilities: [{}],
-  // hostname: '127.0.0.1',
   port: 9519,
   waitforTimeout: 5000,
   connectionRetryCount: 10,
   connectionRetryTimeout: 30000,
   logLevel: 'debug',
   runner: 'local',
-  outputDir: 'all-logs',
+  outputDir: 'wdio-logs',
   specs: ['./test/*.spec.ts'],
   autoCompileOpts: {
     autoCompile: true,
-    // see https://github.com/TypeStrong/ts-node#cli-and-programmatic-options
-    // for all available options
     tsNodeOpts: {
       transpileOnly: true,
       files: true,
-      project: `${__dirname}/tsconfig.json`,
+      project: join(__dirname, 'tsconfig.json'),
     },
-
-    // tsconfig-paths is only used if "tsConfigPathsOpts" are provided, if you
-    // do please make sure "tsconfig-paths" is installed as dependency
-    // tsConfigPathsOpts: {
-    //   baseUrl: './',
-    // },
   },
   framework: 'mocha',
   mochaOpts: {
