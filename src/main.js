4837291eff31abd39d10b49d24c42fe6194d33e6
Sam Maister <goosewobbler@protonmail.com>
2025-02-12 11:24:52 +0000

    fix: update test app

--- src/main.js
+++ src/main.js
@@ -1,46 +1,83 @@
-/* eslint no-console: off, global-require: off */
-const { app, BrowserWindow, ipcMain } = require('electron');
-const { isTest } = require('./util');
+import { app, BrowserWindow, ipcMain, dialog } from 'electron';
+const isTest = process.env.TEST === 'true';
+const isSplashEnabled = Boolean(process.env.ENABLE_SPLASH_WINDOW);
 
 if (isTest) {
   require('wdio-electron-service/main');
 }
 
 const appPath = app.getAppPath();
-
 const appRootPath = `${appPath}/dist`;
-let mainWindow = null;
+const resourcePaths = {
+  preloadJs: `${appRootPath}/preload.js`,
+  splashHtml: `${appRootPath}/splash.html`,
+  indexHtml: `${appRootPath}/index.html`,
+};
 
-app.on('ready', () => {
-  console.log('main log');
-  console.warn('main warn');
-  console.error('main error');
+let mainWindow;
+let splashWindow;
 
-  global.mainProcessGlobal = 'foo';
-  global.ipcEventCount = 0;
-
+const createMainWindow = () => {
   mainWindow = new BrowserWindow({
     x: 25,
     y: 35,
     width: 200,
     height: 300,
     webPreferences: {
-      preload: `${appRootPath}/preload.js`,
-      enableRemoteModule: false,
+      preload: resourcePaths.preloadJs,
+      sandbox: !isTest,
       nodeIntegration: false,
       contextIsolation: true,
     },
   });
-
   mainWindow.on('closed', () => {
-    mainWindow = null;
+    mainWindow.destroy();
   });
-  mainWindow.loadFile(`${appRootPath}/index.html`);
+  mainWindow.loadFile(resourcePaths.indexHtml);
 
   mainWindow.on('ready-to-show', () => {
     mainWindow.title = 'this is the title of the main window';
     // mainWindow.webContents.openDevTools();
   });
+};
+
+const createSplashWindow = () => {
+  splashWindow = new BrowserWindow({
+    x: 25,
+    y: 110,
+    width: 200,
+    height: 200,
+    frame: false,
+    webPreferences: {
+      preload: resourcePaths.preloadJs,
+      sandbox: !isTest,
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+  splashWindow.loadFile(resourcePaths.splashHtml);
+  splashWindow.once('ready-to-show', () => {
+    splashWindow.show();
+  });
+};
+
+app.on('ready', () => {
+  console.log('main log');
+  console.warn('main warn');
+  console.error('main error');
+
+  if (isSplashEnabled) {
+    createSplashWindow();
+
+    // to minimize the E2E test duration, we can switch to the main window programmatically
+    ipcMain.handle('switch-main-window', () => {
+      splashWindow.hide();
+      createMainWindow();
+      splashWindow.destroy();
+    });
+  } else {
+    createMainWindow();
+  }
 
   ipcMain.handle('increase-window-size', () => {
     const bounds = mainWindow.getBounds();
@@ -52,10 +89,14 @@ app.on('ready', () => {
     mainWindow.setBounds({ ...bounds, height: bounds.height - 10, width: bounds.width - 10 });
   });
 
-  // custom main process API
-  ipcMain.handle('wdio-electron', () => 'test');
-});
-
-ipcMain.on('ipc-event', (event, count) => {
-  global.ipcEventCount += count;
+  ipcMain.handle('show-open-dialog', async () => {
+    const result = await dialog.showOpenDialog(mainWindow, {
+      title: 'Select txt',
+      filters: [
+        { name: 'TXT', extensions: ['txt'] },
+        { name: 'All Files', extensions: ['*'] },
+      ],
+    });
+    console.log(result);
+  });
 });
