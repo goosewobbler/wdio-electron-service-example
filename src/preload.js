88bdd46f52a3eabd4e39bc0192f020ddee3a7674
Sam Maister <goosewobbler@protonmail.com>
2025-02-13 17:28:24 +0000

    fix: add missing preload channel

--- src/preload.js
+++ src/preload.js
@@ -6,7 +6,7 @@ if (isTest) {
   require('wdio-electron-service/preload');
 }
 
-const validChannels = ['increase-window-size', 'decrease-window-size'];
+const validChannels = ['increase-window-size', 'decrease-window-size', 'show-open-dialog'];
 
 const invoke = (channel, ...data) =>
   validChannels.includes(channel) ? ipcRenderer.invoke(channel, data) : Promise.reject();
